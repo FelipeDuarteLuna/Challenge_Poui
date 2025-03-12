d5a0de5f896e4a834f7774ac0ba68531702991ea
Felipe Luna <felipinho.luna@gmail.com>
2025-03-12 09:11:35 -0300

    feat(select): add select component with options and styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { SelectComponent } from './shared/select/select.component';
+import { SelectOption } from './models/selectOption';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [SelectComponent, FormsModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Challenge_Poui';
+  title = 'Componente Campo Select - Challenge_Poui';
+
+  countries: SelectOption[] = [
+    { value: 'brazil', label: 'Brazil' },
+    { value: 'usa', label: 'USA' },
+    { value: 'canada', label: 'Canada' }
+  ];
+
+  selectedCountry: string = 'us';
+
+  isDisabled: boolean = true;
+
+  form: FormGroup;
+
+  constructor(private readonly fb: FormBuilder) {
+    this.form = this.fb.group({
+      country: [{ value: this.selectedCountry, disabled: this.isDisabled }]
+    });
+  }
 }
---
+++ src/app/shared/select/select.component.ts
@@ -0,0 +1,61 @@
+import { Component, forwardRef, Input } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { SelectOption } from '../../models/selectOption';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-select',
+  imports: [CommonModule],
+  templateUrl: './select.component.html',
+  styleUrl: './select.component.scss',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SelectComponent),
+      multi: true
+    }
+  ]
+})
+export class SelectComponent implements ControlValueAccessor {
+  @Input() label!: string;
+  @Input() options: SelectOption[] = [];
+  @Input() disabled: boolean = false;
+
+  isOpen: boolean = false;
+  value!: string;
+  selectedLabel: string = '';
+
+  onChange: any = () => {};
+  onTouched: any = () => {};
+
+  writeValue(value: string): void {
+    this.value = value;
+    this.selectedLabel = this.options?.find(option => option.value === value)?.label ?? '';
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  toggleDropdown(): void {
+    if (!this.disabled) {
+      this.isOpen = !this.isOpen;
+    }
+  }
+
+  selectOption(option: SelectOption): void {
+    this.value = option.value;
+    this.selectedLabel = option.label;
+    this.isOpen = false;
+    this.onChange(this.value);
+    this.onTouched();
+  }
+}
