de4628e54b2b08683082aa16f114006f6e60c40c
Felipe Luna <felipinho.luna@gmail.com>
2025-03-17 19:09:34 -0300

    feat(app): enhance app component with select and switch functionalities, including error handling and styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,31 +2,57 @@ import { Component } from '@angular/core';
 import { SelectComponent } from './shared/select/select.component';
 import { SelectOption } from './models/selectOption';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SwitchComponent } from './shared/switch/switch.component';
+import { PoModule, PoPageModule } from '@po-ui/ng-components';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PoTemplatesModule } from '@po-ui/ng-templates';
 
 @Component({
   selector: 'app-root',
-  imports: [SelectComponent, FormsModule, ReactiveFormsModule],
+  imports: [ SelectComponent, SwitchComponent, FormsModule, ReactiveFormsModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Componente Campo Select - Challenge_Poui';
+  title = 'Choose an option';
 
   countries: SelectOption[] = [
-    { value: 'brazil', label: 'Brazil' },
-    { value: 'usa', label: 'USA' },
-    { value: 'canada', label: 'Canada' }
+    { value: 'opt1', label: 'Option 1' },
+    { value: 'opt2', label: 'Option 2' },
+    { value: 'opt3', label: 'Option 3' },
+    { value: 'opt4', label: 'Option 4' },
+    { value: 'opt5', label: 'Option 5' },
   ];
 
   selectedCountry: string = 'us';
 
-  isDisabled: boolean = true;
+  isDisabled: boolean = false;
+  isDisabledSwitch: boolean = false;
+  hasError: boolean = false;
 
   form: FormGroup;
 
   constructor(private readonly fb: FormBuilder) {
     this.form = this.fb.group({
-      country: [{ value: this.selectedCountry, disabled: this.isDisabled }]
+      country: [{ value: this.selectedCountry, disabled: this.isDisabled }],
     });
   }
+
+  DisableSelect(): void {
+    if (this.form.get('country')?.disabled) {
+      this.form.get('country')?.enable();
+      this.isDisabled = false;
+    } else {
+      this.form.get('country')?.disable();
+      this.isDisabled = true;
+    }
+  }
+
+  isActive = false;
+
+  onSwitchToggle(event: boolean) {
+    console.log('Switch mudou:', event);
+  }
+
 }
