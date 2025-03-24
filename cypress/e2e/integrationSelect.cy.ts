820eed11bcefdcd084a2cc616a12d0eb9665209c
Felipe Luna <felipinho.luna@gmail.com>
2025-03-24 20:47:28 -0300

    test(cypress): update select component tests to use force click for better interaction handling

--- cypress/e2e/integrationSelect.cy.ts
+++ cypress/e2e/integrationSelect.cy.ts
@@ -5,29 +5,29 @@ describe('Select Component', () => {
     // Garantir que o select está visível antes de interagir
     cy.get('[ng-reflect-label="Choose an option"] > .custom-select > .select-button')
       .should('be.visible')
-      .click();
+      .click({ force: true });
 
-    cy.get('.select-dropdown > :nth-child(3)').should('be.visible').click();
+    cy.get('.select-dropdown > :nth-child(3)').should('be.visible').click({ force: true });
 
     // Aplicar erro e garantir visibilidade antes de interagir
     cy.get('.buttonError').should('be.visible').click();
-    cy.get('.buttonError').click();
+    cy.get('.buttonError').click({ force: true });
 
     // Selecionar outra opção
     cy.get('form.ng-untouched > .ng-untouched > .custom-select > .select-button')
       .should('be.visible')
-      .click();
-    cy.get('.select-dropdown > :nth-child(5)').should('be.visible').click();
+      .click({ force: true });
+    cy.get('.select-dropdown > :nth-child(5)').should('be.visible').click({ force: true });
 
     // Desativar Select
-    cy.get('#toggle-select').should('be.visible').click();
+    cy.get('#toggle-select').should('be.visible').click({ force: true });
 
     // Verificar se o Select está desativado
     cy.get('[ng-reflect-label="Choose an option"] > .custom-select > .select-button')
       .should('have.css', 'cursor', 'not-allowed');
 
     // Reativar o Select
-    cy.get('#toggle-select').should('be.visible').click();
+    cy.get('#toggle-select').should('be.visible').click({ force: true });
     cy.get('[ng-reflect-label="Choose an option"] > .custom-select > .select-button')
       .should('have.css', 'cursor', 'pointer');
   });
